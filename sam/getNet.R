0d911792960fd070939a238a337074405f25a032
Sam Nuzbrokh <snuzbrokh@gmail.com>
2020-06-10 17:20:40 -0400

    added plots for cross county providers and doctors

--- sam/getNet.R
+++ sam/getNet.R
@@ -1,13 +1,13 @@
 
 
-get_Bnet = function(data, state, county){
+propat = function(data, state, county){
   
   data.filt = 
     data %>% 
-    dplyr::select(State, County, Provider, PotentialFraud, ClaimID, BeneID) %>% 
-    group_by(State, County, Provider, PotentialFraud, BeneID) %>% 
-    tally() %>% 
     filter(State %in% state, County %in% county) %>% 
+    dplyr::select(Provider, PotentialFraud, ClaimID, BeneID) %>% 
+    group_by(Provider, PotentialFraud, BeneID) %>% 
+    tally() %>% 
     rename('weights' = 'n') %>% 
     data.frame()
   
@@ -28,6 +28,58 @@ get_Bnet = function(data, state, county){
   V(Bnet)$actor <- ifelse(V(Bnet)$name %in% connections[,1],'Provider','Patient')
   
   V(Bnet)$fraud <- ifelse(V(Bnet)$name %in% fraud[,1],fraud[,2],'Patient')
+  
+  V(Bnet)$size <- degree(Bnet)
+  
+  # Add node attribute for dead or not
+  
+  #E(Bnet)$weight <- 
+  
+  V(Bnet)$shape <- shapes[V(Bnet)$type+1]
+  
+  # Create 4 new features for connectivity of providers based on bipartite projection graphs based on 
+  # doctor and patient networks
+  
+  return(Bnet)
+}
+
+prodoc = function(data, state, county){
+  if(county == 'all'){
+    county = data %>% 
+      filter(State %in% state) %>% 
+      distinct(County) %>% pull
+  }
+  
+  data.filt = 
+    data %>% 
+    filter(State %in% state, County %in% county) %>% 
+    dplyr::select(Provider, PotentialFraud, ClaimID, 
+           AttendingPhysician, OperatingPhysician, OtherPhysician) %>% 
+    pivot_longer(cols=c(AttendingPhysician,OperatingPhysician,OtherPhysician), 
+                 names_to = "Type", values_to = "Doctor") %>%
+    filter(complete.cases(.)) %>% 
+    group_by(Provider, PotentialFraud, Doctor) %>% 
+    tally() %>% 
+    rename('weights' = 'n') %>% 
+    data.frame()
+  
+  ## Get Provider | Beneficiary
+  connections = data.filt %>% dplyr::select(Provider,Doctor,weights)
+  
+  ## Create bipartite graph
+  Bnet <- graph.data.frame(connections,directed=FALSE)
+  
+  ## Add attributes
+  shapes <- c(21,15)
+  
+  
+  fraud = data.filt %>% select(Provider,PotentialFraud)
+  
+  V(Bnet)$type <- V(Bnet)$name %in% connections[,1]
+  
+  V(Bnet)$actor <- ifelse(V(Bnet)$name %in% connections[,1],'Provider','Patient')
+  
+  V(Bnet)$fraud <- ifelse(V(Bnet)$name %in% fraud[,1],fraud[,2],'Doctor')
   
   V(Bnet)$size <- degree(Bnet)
   
